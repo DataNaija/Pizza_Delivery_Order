58b425f73fd9f8d6faaf76f118a64e8d0d49a50e
DataNaija <139554441+DataNaija@users.noreply.github.com>
2023-08-01 16:20:55 -0600

    Add files via upload

---
+++ dy_32.test.py
@@ -0,0 +1,82 @@
+# Instructions
+# Congratulations, you've got a job at Python Pizza. Your first job is to build an automatic pizza order program. 
+# Based on a user's order, work out their final bill. 
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
+#  Example Input
+# size = "L"
+# add_pepperoni = "Y"
+# extra_cheese = "N"
+# Example Output
+# Your final bill is: $28.
+# e.g. When you hit **run**, this is what should happen:  
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L:  ")
+add_pepperoni = input("Do you want pepperoni? Y or N:  ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+if size == "S":
+  size = 15
+  if add_pepperoni == "Y":
+    p_small = 2
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + p_small + extra
+      print(f'Your final bill is: ${bill}')
+    if extra_cheese == "N":
+      bill = size + p_small
+      print(f'Your final bill is: ${bill}')
+  if add_pepperoni == "N":
+    if extra_cheese == "N":
+      bill = size
+      print(f'Your final bill is: ${size}')
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + extra
+      print(f'Your final bill is: ${bill}')
+
+ 
+if size == "M":
+  size = 20
+  if add_pepperoni == "Y":
+    p_medium = 3
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + p_medium + extra
+      print(f'Your final bill is: ${bill}')
+    if extra_cheese == "N":
+      bill = size + p_medium
+      print(f'Your final bill is: ${bill}')
+  if add_pepperoni == "N":
+    if extra_cheese == "N":
+      bill = size
+      print(f'Your final bill is: ${size}')
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + extra
+      print(f'Your final bill is: ${bill}')
+
+if size == "L":
+  size = 25
+  if add_pepperoni == "Y":
+    p_large = 3
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + p_large + extra
+      print(f'Your final bill is: ${bill}')
+    if extra_cheese == "N":
+      bill = size + p_large
+      print(f'Your final bill is: ${bill}')
+  if add_pepperoni == "N":
+    if extra_cheese == "N":
+      bill = size
+      print(f'Your final bill is: ${bill}')
+    if extra_cheese == "Y":
+      extra = 1
+      bill = size + extra
+      print(f'Your final bill is: ${bill}')
